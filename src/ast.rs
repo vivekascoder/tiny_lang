e04da78b1f0007c2c71ec83684ddad873da923f6
vivekascoder <vivekascoder@gmail.com>
2023-07-01 16:07:44 +0530

    feat(lexer): next_token

--- src/ast.rs
+++ src/ast.rs
@@ -38,104 +38,104 @@ pub struct Parser {
     cur: usize,
 }
 
-impl Parser {
-    pub fn new(source: &str) -> Self {
-        let mut tokenizer = Tokenizer::new(source);
-        Self {
-            source: source.to_string(),
-            lexer: tokenizer.tokenize(),
-            cur: 0,
-        }
-    }
+// impl Parser {
+//     pub fn new(source: &str) -> Self {
+//         let mut tokenizer = Tokenizer::new(source);
+//         Self {
+//             source: source.to_string(),
+//             lexer: tokenizer.next_token(),
+//             cur: 0,
+//         }
+//     }
 
-    fn parse_let_statement(&self) -> Result<LetStatement> {
-        // make sure the current token is a let keyword.
-        if !(self.lexer[self.cur].type_ == TokenType::KeywordLet) {
-            bail!(
-                "Let statement parsing error, the current token is {:?} and not KeywordLet",
-                self.lexer[self.cur]
-            );
-        }
+//     fn parse_let_statement(&self) -> Result<LetStatement> {
+//         // make sure the current token is a let keyword.
+//         if !(self.lexer[self.cur].type_ == TokenType::KeywordLet) {
+//             bail!(
+//                 "Let statement parsing error, the current token is {:?} and not KeywordLet",
+//                 self.lexer[self.cur]
+//             );
+//         }
 
-        // The next token should be an identifier.
-        let name = match &self.lexer[self.cur + 1].type_ {
-            TokenType::Identifier(name) => name.clone(),
-            _ => {
-                bail!(
-                    "Let statement parsing error, can't find the `=` operator, found {:?} instead.",
-                    self.lexer[self.cur + 1]
-                );
-            }
-        };
+//         // The next token should be an identifier.
+//         let name = match &self.lexer[self.cur + 1].type_ {
+//             TokenType::Identifier(name) => name.clone(),
+//             _ => {
+//                 bail!(
+//                     "Let statement parsing error, can't find the `=` operator, found {:?} instead.",
+//                     self.lexer[self.cur + 1]
+//                 );
+//             }
+//         };
 
-        // The next token should be an colon.
-        if !(self.lexer[self.cur + 1].type_ == TokenType::Colon) {
-            bail!(
-                "Let statement parsing error, can't find the `:` operator, found {:?} instead.",
-                self.lexer[self.cur + 1]
-            );
-        }
+//         // The next token should be an colon.
+//         if !(self.lexer[self.cur + 1].type_ == TokenType::Colon) {
+//             bail!(
+//                 "Let statement parsing error, can't find the `:` operator, found {:?} instead.",
+//                 self.lexer[self.cur + 1]
+//             );
+//         }
 
-        // // The next token should be an keyword type.
-        // if let TokenType::Identifier(name) = self.lexer[self.cur + 1].type_ {
-        // } else {
-        //     bail!(
-        //         "Let statement parsing error, can't find the `=` operator, found {:?} instead.",
-        //         self.lexer[self.cur + 1]
-        //     );
-        // }
+//         // // The next token should be an keyword type.
+//         // if let TokenType::Identifier(name) = self.lexer[self.cur + 1].type_ {
+//         // } else {
+//         //     bail!(
+//         //         "Let statement parsing error, can't find the `=` operator, found {:?} instead.",
+//         //         self.lexer[self.cur + 1]
+//         //     );
+//         // }
 
-        // The next token should be `=`.
-        if !(self.lexer[self.cur + 1].type_ == TokenType::Equal) {
-            bail!(
-                "Let statement parsing error, can't find the `=` operator, found {:?} instead.",
-                self.lexer[self.cur + 1]
-            );
-        }
+//         // The next token should be `=`.
+//         if !(self.lexer[self.cur + 1].type_ == TokenType::Equal) {
+//             bail!(
+//                 "Let statement parsing error, can't find the `=` operator, found {:?} instead.",
+//                 self.lexer[self.cur + 1]
+//             );
+//         }
 
-        // The next token should be an `Expression`.
-        // Since we support only `Number` RN let's parse that.
-        // Ok(LetStatement {
-        //     token: self.lexer[self.cur],
-        //     identifier: name,
-        //     value: ,
-        // })
-        todo!();
-    }
+//         // The next token should be an `Expression`.
+//         // Since we support only `Number` RN let's parse that.
+//         // Ok(LetStatement {
+//         //     token: self.lexer[self.cur],
+//         //     identifier: name,
+//         //     value: ,
+//         // })
+//         todo!();
+//     }
 
-    fn parse_statements(&self) -> Result<Vec<Statement>> {
-        let mut statements: Vec<Statement> = vec![];
-        match self.lexer[self.cur].type_ {
-            TokenType::KeywordLet => {
-                // Parse let statement
-                statements.push(Statement::Let(self.parse_let_statement()?));
-            }
-            _ => {
-                bail!("Can't parse this token type: {:?}", self.lexer[self.cur]);
-            }
-        }
-        Ok(statements)
-    }
+//     fn parse_statements(&self) -> Result<Vec<Statement>> {
+//         let mut statements: Vec<Statement> = vec![];
+//         match self.lexer[self.cur].type_ {
+//             TokenType::KeywordLet => {
+//                 // Parse let statement
+//                 statements.push(Statement::Let(self.parse_let_statement()?));
+//             }
+//             _ => {
+//                 bail!("Can't parse this token type: {:?}", self.lexer[self.cur]);
+//             }
+//         }
+//         Ok(statements)
+//     }
 
-    fn parse_expression(&self) -> Result<Expression> {
-        todo!()
-    }
+//     fn parse_expression(&self) -> Result<Expression> {
+//         todo!()
+//     }
 
-    pub fn parse(&self) {
-        loop {
-            if self.cur == self.lexer.len() {
-                break;
-            }
+//     pub fn parse(&self) {
+//         loop {
+//             if self.cur == self.lexer.len() {
+//                 break;
+//             }
 
-            // Recursive descennt parser
-            match self.lexer[self.cur].type_ {
-                TokenType::KeywordLet => {
-                    // Parse let statement
-                }
-                _ => {
-                    todo!("Hmm")
-                }
-            }
-        }
-    }
-}
+//             // Recursive descennt parser
+//             match self.lexer[self.cur].type_ {
+//                 TokenType::KeywordLet => {
+//                     // Parse let statement
+//                 }
+//                 _ => {
+//                     todo!("Hmm")
+//                 }
+//             }
+//         }
+//     }
+// }
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,5 @@
+use anyhow::{bail, Ok, Result};
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum TokenType {
     EOF,
@@ -55,120 +57,120 @@ impl Tokenizer {
     fn parse_keyword(&self, s: &str) -> Option<TokenType> {
         match s {
             "let" => Some(TokenType::KeywordLet),
+            "usize" => Some(TokenType::KeywordUsize),
             _ => None,
         }
     }
 
-    pub fn tokenize(&mut self) -> Vec<Token> {
-        let mut tokens: Vec<Token> = vec![];
+    fn skip_whitespace(&mut self) {
         loop {
-            if self.cur == self.source.len() {
-                break;
-            }
-
-            println!("{:?}", self.source[self.cur]);
-
             match self.source[self.cur] {
-                ';' => {
-                    tokens.push(self.token(TokenType::SemiColon, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                ':' => {
-                    tokens.push(self.token(TokenType::Colon, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                '=' => {
-                    tokens.push(self.token(TokenType::Equal, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                '+' => {
-                    tokens.push(self.token(TokenType::Plus, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                '-' => {
-                    tokens.push(self.token(TokenType::Minus, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                '*' => {
-                    tokens.push(self.token(TokenType::Multiply, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                '/' => {
-                    tokens.push(self.token(TokenType::Divide, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                '%' => {
-                    tokens.push(self.token(TokenType::Mod, (self.cur, self.cur + 1)));
-                    self.cur += 1;
-                }
-                'a'..='z' | 'A'..='Z' | '_' => {
-                    // Identifier
-                    let start = self.cur;
-
-                    // Start a loop to parse the identifier.
-                    loop {
-                        match self.source[self.cur] {
-                            // TODO: Should break if EOF?
-                            'a'..='z' | 'A'..='Z' | '_' | '0'..='9' => {
-                                self.cur += 1;
-                            }
-
-                            // Encountered something else
-                            // Then it could be either identifier or keyword.
-                            _ => {
-                                let slice = &self.source[start..self.cur];
-                                let identifier: String = slice.iter().collect();
-
-                                if let Some(keyword) = self.parse_keyword(&identifier) {
-                                    tokens.push(self.token(keyword, (start, self.cur)));
-                                } else {
-                                    tokens.push(self.token(
-                                        TokenType::Identifier(identifier),
-                                        (start, self.cur),
-                                    ));
-                                }
-                                break;
-                            }
-                        }
-                    }
-                }
-                '0'..='9' => {
-                    // Number
-                    let start = self.cur;
-
-                    loop {
-                        match self.source[self.cur] {
-                            '0'..='9' => {
-                                self.cur += 1;
-                            }
-
-                            // Encountered any other char.
-                            _ => {
-                                let s_no = &self.source[start..self.cur];
-                                let number: usize =
-                                    s_no.iter().collect::<String>().parse().unwrap();
-
-                                tokens
-                                    .push(self.token(TokenType::Usize(number), (start, self.cur)));
-                                break;
-                            }
-                        }
-                    }
-                }
-
                 // Do nothing for whitespace
                 ' ' | '\t' | '\n' | '\r' => {
                     self.cur += 1;
                 }
-
                 _ => {
-                    unreachable!("With: {:?}", self.source[self.cur])
+                    break;
                 }
             }
         }
+    }
 
-        // Finally return the tokens
-        tokens
+    pub fn next_token(&mut self) -> Result<Token> {
+        let mut tokens: Vec<Token> = vec![];
+        if self.cur == self.source.len() {
+            bail!("End of file reaced.");
+        }
+
+        // Skip all the whitespaces till the next token.
+        self.skip_whitespace();
+
+        match self.source[self.cur] {
+            ';' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::SemiColon, (self.cur - 1, self.cur)))
+            }
+            ':' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Colon, (self.cur - 1, self.cur)))
+            }
+            '=' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Equal, (self.cur - 1, self.cur)))
+            }
+            '+' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Plus, (self.cur - 1, self.cur)))
+            }
+            '-' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Minus, (self.cur - 1, self.cur)))
+            }
+            '*' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Multiply, (self.cur - 1, self.cur)))
+            }
+            '/' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Divide, (self.cur - 1, self.cur)))
+            }
+            '%' => {
+                self.cur += 1;
+                Ok(self.token(TokenType::Mod, (self.cur, self.cur + 1)))
+            }
+            'a'..='z' | 'A'..='Z' | '_' => {
+                // Identifier
+                let start = self.cur;
+
+                // Start a loop to parse the identifier.
+                loop {
+                    match self.source[self.cur] {
+                        // TODO: Should break if EOF?
+                        'a'..='z' | 'A'..='Z' | '_' | '0'..='9' => {
+                            self.cur += 1;
+                        }
+
+                        // Encountered something else
+                        // Then it could be either identifier or keyword.
+                        _ => {
+                            let slice = &self.source[start..self.cur];
+                            let identifier: String = slice.iter().collect();
+
+                            if let Some(keyword) = self.parse_keyword(&identifier) {
+                                return Ok(self.token(keyword, (start, self.cur)));
+                            } else {
+                                return Ok(self
+                                    .token(TokenType::Identifier(identifier), (start, self.cur)));
+                            }
+                        }
+                    }
+                }
+            }
+            '0'..='9' => {
+                // Number
+                let start = self.cur;
+
+                loop {
+                    match self.source[self.cur] {
+                        '0'..='9' => {
+                            self.cur += 1;
+                        }
+
+                        // Encountered any other char.
+                        _ => {
+                            let s_no = &self.source[start..self.cur];
+                            let number: usize = s_no.iter().collect::<String>().parse().unwrap();
+
+                            return Ok(self.token(TokenType::Usize(number), (start, self.cur)));
+                        }
+                    }
+                }
+            }
+
+            _ => {
+                unreachable!("With: {:?}", self.source[self.cur])
+            }
+        }
     }
 }
 
@@ -176,97 +178,98 @@ impl Tokenizer {
 pub mod tests {
     use super::TokenType::*;
     use super::*;
+    use std::result::Result::Ok;
 
     #[test]
     fn test_parsing_let_syntax() {
         let source = "let some_var: usize = 345 + 35353;";
         let mut tokenizer = Tokenizer::new(source);
-        let tokens = tokenizer.tokenize();
-        println!("tokens: {:?}", &tokens);
 
-        assert_eq!(
-            tokens,
-            vec![
-                Token {
-                    type_: KeywordLet,
-                    pos: (0, 3)
-                },
-                Token {
-                    type_: TokenType::Identifier(format!("some_var")),
+        let result = vec![
+            Token {
+                type_: KeywordLet,
+                pos: (0, 3),
+            },
+            Token {
+                type_: TokenType::Identifier(format!("some_var")),
 
-                    pos: (4, 12)
-                },
-                Token {
-                    type_: TokenType::Colon,
-                    pos: (12, 13)
-                },
-                Token {
-                    type_: KeywordUsize,
-                    pos: (14, 20)
-                },
-                Token {
-                    type_: TokenType::Equal,
-                    pos: (21, 22)
-                },
-                Token {
-                    type_: TokenType::Usize(345),
-                    pos: (23, 26)
-                },
-                Token {
-                    type_: TokenType::Plus,
-                    pos: (27, 28)
-                },
-                Token {
-                    type_: TokenType::Usize(35353),
-                    pos: (29, 34)
-                },
-                Token {
-                    type_: TokenType::SemiColon,
-                    pos: (34, 35)
-                }
-            ]
-        );
+                pos: (4, 12),
+            },
+            Token {
+                type_: TokenType::Colon,
+                pos: (12, 13),
+            },
+            Token {
+                type_: KeywordUsize,
+                pos: (14, 19),
+            },
+            Token {
+                type_: TokenType::Equal,
+                pos: (20, 21),
+            },
+            Token {
+                type_: TokenType::Usize(345),
+                pos: (22, 25),
+            },
+            Token {
+                type_: TokenType::Plus,
+                pos: (26, 27),
+            },
+            Token {
+                type_: TokenType::Usize(35353),
+                pos: (28, 33),
+            },
+            Token {
+                type_: TokenType::SemiColon,
+                pos: (33, 34),
+            },
+        ];
+        let mut i = 0;
+        while let Ok(token) = tokenizer.next_token() {
+            assert_eq!(token, result[i]);
+            i += 1;
+        }
     }
 
-    fn test_parsing_let_without_type() {
-        let source = "let some_var = 345 + 35353;";
-        let mut tokenizer = Tokenizer::new(source);
-        let tokens = tokenizer.tokenize();
-        println!("tokens: {:?}", &tokens);
+    // fn test_parsing_let_without_type() {
+    //     let source = "let some_var = 345 + 35353;";
+    //     let mut tokenizer = Tokenizer::new(source);
+    //     let tokens = tokenizer.tokenize();
+    //     println!("tokens: {:?}", &tokens);
 
-        assert_eq!(
-            tokens,
-            vec![
-                Token {
-                    type_: KeywordLet,
-                    pos: (0, 3)
-                },
-                Token {
-                    type_: TokenType::Identifier(format!("some_var")),
+    //     assert_eq!(
+    //         tokens,
+    //         vec![
+    //             Token {
+    //                 type_: KeywordLet,
+    //                 pos: (0, 3)
+    //             },
+    //             Token {
+    //                 type_: TokenType::Identifier(format!("some_var")),
 
-                    pos: (4, 12)
-                },
-                Token {
-                    type_: TokenType::Equal,
-                    pos: (21, 22)
-                },
-                Token {
-                    type_: TokenType::Usize(345),
-                    pos: (23, 26)
-                },
-                Token {
-                    type_: TokenType::Plus,
-                    pos: (27, 28)
-                },
-                Token {
-                    type_: TokenType::Usize(35353),
-                    pos: (29, 34)
-                },
-                Token {
-                    type_: TokenType::SemiColon,
-                    pos: (34, 35)
-                }
-            ]
-        );
-    }
+    //                 pos: (4, 12)
+    //             },
+    //             Token {
+    //                 type_: TokenType::Equal,
+    //                 pos: (21, 22)
+    //             },
+    //             Token {
+    //                 type_: TokenType::Usize(345),
+    //                 pos: (23, 26)
+    //             },
+    //             Token {
+    //                 type_: TokenType::Plus,
+    //                 pos: (27, 28)
+    //             },
+    //             Token {
+    //                 type_: TokenType::Usize(35353),
+    //                 pos: (29, 34)
+    //             },
+    //             Token {
+    //                 type_: TokenType::SemiColon,
+    //                 pos: (34, 35)
+    //             }
+    //         ]
+    //     );
+    // }
 }
